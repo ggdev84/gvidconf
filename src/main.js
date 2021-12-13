96732261252632a73981bab100dd0a3acf1fcad3
ggdev84 <gorkemguldev@gmail.com>
2021-12-13 04:41:21 +0100

    Main page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main(){
     return(
         <div className="main">
-            <p>Hello</p>
+            <p>Main page</p>
         </div>
     )
 }
